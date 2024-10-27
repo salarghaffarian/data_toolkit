cc854071e0b28342614d7b9d3b82b503fa4e60d7
Salar Ghaffarian <salarghaffarian1363@gmail.com>
2024-10-27 03:46:15 -0600

    Fix a bug in the writer.py

--- data_toolkit/io/writer.py
+++ data_toolkit/io/writer.py
@@ -5,9 +5,8 @@ Description: This module contains the functions that are used to write data.
 
 from data_toolkit.core.operations import log
 
-def dwrite_data(file_pat, data):
+def write_data(file_pat, data):
     with open(file_path, 'w') as f:
         f.write(data)
         
     log(f"Dat writtent to {file_path}")
-    
